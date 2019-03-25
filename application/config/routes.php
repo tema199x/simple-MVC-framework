fcc4055c7bca0ee05fb795fb23264019c7d055b6
Artyom Nikolaevich <artem.nikolaew199x@mail.ru>
2019-03-25 20:42:05 +0300

    Add add, math and run to Router.php

---
+++ application/config/routes.php
@@ -0,0 +1,16 @@
+<?php 
+
+// page array
+return [
+
+    'account/login' => [
+        'controller' => 'account',
+        'action' => 'login',
+    ],
+
+    'news/show' => [
+        'controller' => 'news',
+        'action' => 'show',
+    ],
+
+];
--- application/core/Router.php
+++ application/core/Router.php
@@ -9,22 +9,46 @@ class Router {
     protected $params = [];
 
     public function __construct() {
-        //
+        $arr = require 'application/config/routes.php';
+        // bust $route and $params
+        foreach ($arr as $key => $val) {
+            // to all methods
+            $this->add($key, $val);
+        }
     }
 
     // add route
-    public function add() {
-        //
+    public function add($route, $params) {
+        $route = '#^'.$route.'$#';
+        $this->routes[$route] = $params;
     }
 
     // check route
     public function math() {
-        //
+        $url = trim($_SERVER['REQUEST_URI'], '/');  
+        foreach ($this->routes as $route => $params) {
+            // если сработало true иначе false
+            if (preg_match($route, $url, $matches)) {
+                $this->params = $params;
+                return true;
+                // var_dump($matches);
+            }
+        }
     }
 
     // start route
     public function run() {
-        echo 'start..';
+        if ($this->math()) {
+            $controller = 'application\controllers\\'.ucfirst($this->params['controller']).'Controller.php';
+            if (class_exists($controller)) {
+                echo 'Ок';
+            } else { 
+                echo 'Не найден '.$controller;
+            }
+            // echo $controller;
+        } else {
+            echo 'Маршрут не найден';
+        }
     }
 
 }
