eafcf59979b40e11bfd1d02f4ef38f32b2e6090c
Artyom Nikolaevich <artem.nikolaew199x@mail.ru>
2019-04-20 14:03:09 +0300

    SQL Injection protection

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -8,14 +8,6 @@ use application\lib\Db;
 class MainController extends Controller {
 
     public function indexAction() {
-
-        $db = new Db;
-
-        // $form = 
-
-        $data = $db->row('SELECT name FROM users WHERE id = 2');
-        debug($data);
-        
         $this->view->render('Главная');
     }
 
