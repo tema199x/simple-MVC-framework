9ab38be5be8e19e4cca8579948628018c616dda7
Artyom Nikolaevich <artem.nikolaew199x@mail.ru>
2019-04-20 14:52:08 +0300

    Connection form.js and jquery.js

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $title; ?></title>
+    <script src="/public/scripts/jquery.js"></script>
+    <script src="/public/scripts/form.js"></script>
 </head>
 <body>
 
